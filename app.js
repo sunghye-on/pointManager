255a2512424d9aa47c8563603881122116faa20d
sunghye-on <opei71@naver.com>
2020-03-20 15:08:14 +0900

    controller생성

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(helmet());
 //로그정보를 찍어 주는 morgan
 app.use(morgan("dev"));
 //해당 URL이 요청되고 해당하는 함수들이 호출된다.
-app.get("/", globalRouter);
-app.get("/user", userRouter);
+app.get(routes.home, globalRouter);
+app.get(routes.users, userRouter);
 
 export default app;
---
+++ controller/userC.js
@@ -0,0 +1,8 @@
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
+
+export const users = (req, res) => res.send("User");
+export const userDetail = (req, res) => res.send("UserDetail");
+export const editProfile = (req, res) => res.send("editProfile");
+export const changePassword = (req, res) => res.send("changePassword");
